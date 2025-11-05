cc395a566ca23f47dec1a83091005dfbc3c9be74
Shaifur-Shaoun <shaounnsu241@gmail.com>
2025-11-06 02:12:57 +0600

    Degree

---
+++ Degree.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    double celsius, fahrenheit;
+
+    cin>>fahrenheit;
+    celsius = (fahrenheit - 32) * 5.0 / 9.0;
+
+    cout<<celsius<<endl;
+
+
+}
