02522ac8018fdb43bc1ae2b6dd12489da56fdb59
Shaifur-Shaoun <shaounnsu241@gmail.com>
2025-12-11 01:47:10 +0600

    Practise

---
+++ Check.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+double seriesSum(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return 1.0 / pow(2, n) + seriesSum(n - 1);
+}
+
+int main() {
+
+  
+    int a[10] = {10, 20, 30, 40, 5, 6, 7, 8, 90, 10};
+      cout << "Sum of the series: " << seriesSum(2) << endl;
+
+    return 0;
+}
+
+
